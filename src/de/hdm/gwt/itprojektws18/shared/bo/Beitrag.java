bf7a1f1f5ee6b85faf544a86776be39b5b3c7bef
Marko Deveric <markodeveric@icloud.com>
2018-11-27 15:50:42 +0100

    Implementierung PinnwandVerwaltungImpl + Anpassung BO Beitrag

--- src/de/hdm/gwt/itprojektws18/shared/bo/Beitrag.java
+++ src/de/hdm/gwt/itprojektws18/shared/bo/Beitrag.java
@@ -4,5 +4,26 @@ public class Beitrag extends Textbeitrag{
 	
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Fremdschluesselbeziehung zur Pinnwand
+	 */
+	
+	private int zielId;
 
+	/**
+	 * Auslesen des Fremdschluessels zur Pinnwand
+	 * @return zielId
+	 */
+	public int getZielId() {
+		return zielId;
+	}
+
+	/**
+	 * Setzen des Fremdschluessels zur Pinnwand
+	 * @param zielId
+	 */
+	public void setZielId(int zielId) {
+		this.zielId = zielId;
+	}
+	
 }
