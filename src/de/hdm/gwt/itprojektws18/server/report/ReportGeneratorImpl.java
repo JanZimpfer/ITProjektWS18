67da1f2ffa14ccf10c790b487a943c3e2af79889
AyseGuelay <ag122@hdm-stuttgart.de>
2018-11-29 09:44:12 +0100

    Implementierung ReportGeneratorImpl
    Methoden:
    init
    createNutzerStatistikReport
    createBeitragStatistikReport

--- src/de/hdm/gwt/itprojektws18/server/report/ReportGeneratorImpl.java
+++ src/de/hdm/gwt/itprojektws18/server/report/ReportGeneratorImpl.java
@@ -1,5 +1,40 @@
 package de.hdm.gwt.itprojektws18.server.report;
 
-public class ReportGeneratorImpl {
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.gwt.itprojektws18.server.PinnwandVerwaltungImpl;
+import de.hdm.gwt.itprojektws18.shared.report.*;
+import de.hdm.gwt.itprojektws18.shared.bo.*;
+import de.hdm.gwt.itprojektws18.shared.ReportGenerator;
+
+
+@SuppressWarnings("serial")
+
+public class ReportGeneratorImpl extends RemoteServiceServlet 
+
+implements ReportGenerator{
+	
+
+	
+	public ReportGeneratorImpl() throws IllegalArgumentException{
+		
+	}
+	
+	
+	public void init() throws IllegalArgumentException{
+		
+	}
+	
+	
+	public nutzerStatistikReport createNutzerStatistikReport(Nutzer nutzer) {
+		
+	}
+	
+	
+	public beitragStatistikReport createBeitragStatistikReport(Beitrag beitrag) {
+		
+	}
+
+	
+	
 }
