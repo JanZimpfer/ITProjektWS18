b5576631462aa3c87a2bb07704fea7019dbec27a
AyseGuelay <ag122@hdm-stuttgart.de>
2018-11-29 16:12:46 +0100

    Implementierung ReportGenerator
    + Erstellung Report- Klassen

--- src/de/hdm/gwt/itprojektws18/server/report/ReportGeneratorImpl.java
+++ src/de/hdm/gwt/itprojektws18/server/report/ReportGeneratorImpl.java
@@ -5,35 +5,53 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import de.hdm.gwt.itprojektws18.server.PinnwandVerwaltungImpl;
 import de.hdm.gwt.itprojektws18.shared.report.*;
 import de.hdm.gwt.itprojektws18.shared.bo.*;
+import de.hdm.gwt.itprojektws18.shared.PinnwandVerwaltung;
 import de.hdm.gwt.itprojektws18.shared.ReportGenerator;
 
 
 @SuppressWarnings("serial")
 
-public class ReportGeneratorImpl extends RemoteServiceServlet 
-
-implements ReportGenerator{
+public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportGenerator {
 	
-
+	private PinnwandVerwaltung pinnwandAdmin = null;
 	
 	public ReportGeneratorImpl() throws IllegalArgumentException{
 		
 	}
 	
-	
+	@Override
 	public void init() throws IllegalArgumentException{
+		PinnwandVerwaltungImpl pinnwandVerwaltung = new PinnwandVerwaltungImpl();
+		pinnwandVerwaltung.init();
+		this.pinnwandAdmin = pinnwandVerwaltung;
+	}
+	
+	protected PinnwandVerwaltung getPinnwandVerwaltung() {
+		return this.pinnwandAdmin;
+	}
+	@Override
+	public NutzerStatistikReport createNutzerStatistikReport(Nutzer nutzer) throws IllegalArgumentException {
 		
+	if(this.getPinnwandVerwaltung() == null) {
+		return null;
+	}
+	else {
+		
+		//Vector <Nutzer> alleNutzer = new Vector <Nutzer>();
+		
+		
+		
+	}
+	return null;
+	
+	}
+	
+	@Override
+	public BeitragStatistikReport createBeitragStatistikReport(Beitrag beitrag) throws IllegalArgumentException{
+		return null;
 	}
 	
 	
-	public nutzerStatistikReport createNutzerStatistikReport(Nutzer nutzer) {
-		
-	}
-	
-	
-	public beitragStatistikReport createBeitragStatistikReport(Beitrag beitrag) {
-		
-	}
 
 	
 	
