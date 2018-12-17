18aeda6975c786faeec88e930aaec0633cdafae0
JanZimpfer <jan.zimpfer@gmx.de>
2018-12-17 20:50:13 +0100

    ProfilBox erstellt

--- src/de/hdm/gwt/itprojektws18/client/gui/ProfilBox.java
+++ src/de/hdm/gwt/itprojektws18/client/gui/ProfilBox.java
@@ -1,12 +1,59 @@
 package de.hdm.gwt.itprojektws18.client.gui;
 
-public class ProfilBox {
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.*;
+
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+public class ProfilBox extends HorizontalPanel {
+	
+	private HorizontalPanel ProfilBox = new HorizontalPanel ();
+		
+	private Button  profilbildButton = new Button ("Mein Profilbild");
+	private Button 	profilButton = new Button ("Mein Profil");
+	private Label 	beitraege = new Label ();
+	private Label 	abonniert = new Label ();
+	private Label 	abonnenten = new Label ();
+	
 	
 	public ProfilBox() {
-		
 	}
 	
+	
+	
 	public void onLoad() {
+		
+	 this.add(ProfilBox);
+	 
+	 ProfilBox.add(beitraege);
+	 ProfilBox.add(abonniert);
+	 ProfilBox.add(abonnenten);
+	 
+	 ProfilBox.add(profilButton);
+	 ProfilBox.add(profilbildButton);
+	 
+	 profilButton.addClickHandler(new ClickHandler ()  {
+		 
+		 public void onClick(ClickEvent event) {
+			 
+		 }
+	 }	 
+			 );
+	 
+	 profilbildButton.addClickHandler(new ClickHandler() {
+		 
+		 public void onClick(ClickEvent event) {
+			 
+			 
+		 		}
+	 	}
+			 
+	);
+	 
+	 
+	 
+	
 		
 	}
 
