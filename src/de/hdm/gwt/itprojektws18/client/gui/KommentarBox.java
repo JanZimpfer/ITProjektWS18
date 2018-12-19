97b7a85c6af5b559aa21317c9100cd495994485d
MarkoDeveric <markodeveric@icloud.com>
2018-12-20 00:39:12 +0100

    Implementierung KommentarBox begonnen

--- src/de/hdm/gwt/itprojektws18/client/gui/KommentarBox.java
+++ src/de/hdm/gwt/itprojektws18/client/gui/KommentarBox.java
@@ -1,12 +1,51 @@
 package de.hdm.gwt.itprojektws18.client.gui;
 
-public class KommentarBox {
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class KommentarBox extends FlowPanel{
+	
+	/*
+	 * Elemente zur Darstellung der KommentarBox
+	 */
+	
+	private VerticalPanel kommentarPanel = new VerticalPanel();
+	
+	private Widget kommentarInhalt = new Widget();
+	private Widget nickname = new Widget();
+	private Widget vorname = new Widget();
+	private Widget nachname = new Widget();
+	private Widget erstellZeitpunkt = new Widget();
+	
+	private Button submitKommentarBtn = new Button("Kommentieren");
+	
+	
+	
+	
 	
 	public KommentarBox() {
-		
+	
 	}
 
 	public void onLoad() {
+		
+		//Stylename fuer alle KommentarBoxen
+		this.addStyleName("kommentarBox");
+		
+		//Hinzufuegen der Widgets
+		kommentarPanel.add(kommentarInhalt);
+		kommentarPanel.add(nickname);
+		kommentarPanel.add(vorname);
+		kommentarPanel.add(nachname);
+		kommentarPanel.add(erstellZeitpunkt);
+		
+		//Buttons hinzufuegen
+		kommentarPanel.add(submitKommentarBtn);
+		
+		//Styling hinzufuegen
+		submitKommentarBtn.addStyleName("submitButton");
 		
 	}
 }
