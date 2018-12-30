a06de72bedbe8cdca4f7a0cee564ddf1855e58b8
JanZimpfer <jan.zimpfer@gmx.de>
2018-12-30 15:16:37 +0100

    GUI Anpassung:
    -Suchleistenfunktion implementiert

--- src/de/hdm/gwt/itprojektws18/client/gui/Suchleiste.java
+++ src/de/hdm/gwt/itprojektws18/client/gui/Suchleiste.java
@@ -2,15 +2,18 @@ package de.hdm.gwt.itprojektws18.client.gui;
 
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SuggestBox;
 
 public class Suchleiste extends HorizontalPanel {
 	
 	private HorizontalPanel suchleiste = new HorizontalPanel ();
-	private	SuggestBox textfeld = new SuggestBox ();
-	private Button 	sucheButton = new Button ("Suche!");
+	private MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
+	private Button sucheButton = new Button ("Suche");
+	private SuggestBox textfeld = new SuggestBox (oracle);
 	
+
 	
 	public Suchleiste () {
 		
@@ -18,17 +21,30 @@ public class Suchleiste extends HorizontalPanel {
 	suchleiste.setCellHorizontalAlignment(suchleiste, ALIGN_LEFT);
 	suchleiste.setCellHorizontalAlignment(sucheButton, ALIGN_RIGHT);
 	
+	
+		oracle.add("Flo");
+		oracle.add("Nik");
+		oracle.add("Matthias");
+		oracle.add("Marko");
+		oracle.add("Ayse");
+		oracle.add("Jan");
+		
+	
+	
 	}
 	
 	public void onLoad() {
+	
 		
 	this.addStyleName("suchleiste");
 	this.add(suchleiste);
 	
+	
 	suchleiste.add(textfeld);
 	suchleiste.add(sucheButton);
 
-
+	
+	
 	
 	sucheButton.addStyleName("suchButton");
 	textfeld.addStyleName("suchleiste");
