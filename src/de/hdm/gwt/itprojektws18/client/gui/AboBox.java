468cd6c9b2d7969323004f8e37f930344ec6c80f
NiklasFuchs <nf028@hdm-stuttgart.de>
2019-01-07 11:20:59 +0100

    GUI Implementierungen
    - Arbeit in AboBox (Erstellen eines Abos möglich)
    - Arbeit in LikeBox

--- src/de/hdm/gwt/itprojektws18/client/gui/AboBox.java
+++ src/de/hdm/gwt/itprojektws18/client/gui/AboBox.java
@@ -2,14 +2,18 @@ package de.hdm.gwt.itprojektws18.client.gui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
 
 import de.hdm.gwt.itprojektws18.client.ClientsideSettings;
+import de.hdm.gwt.itprojektws18.client.gui.AboPinnwandBox.PinnwandAnzeigen;
 import de.hdm.gwt.itprojektws18.shared.PinnwandVerwaltungAsync;
 import de.hdm.gwt.itprojektws18.shared.bo.Abonnement;
 import de.hdm.gwt.itprojektws18.shared.bo.Nutzer;
 import de.hdm.gwt.itprojektws18.shared.bo.Pinnwand;
 
+import java.sql.Timestamp;
 import java.util.Vector;
 
 
@@ -65,6 +69,9 @@ public class AboBox extends VerticalPanel{
 
 	public void onLoad() {
 		
+		/**
+		 * 
+		 */
 		this.addStyleName("abobox");
 		this.add(abobox);
 		
@@ -77,14 +84,17 @@ public class AboBox extends VerticalPanel{
 		profilbereich.add(deAboBtn);
 		
 		aboniertePW.add(anzeigePW);
-		aboniertePW.add(aboPinnwandInfos);
+//		aboniertePW.add(aboPinnwandInfos);
 		
-		aboPinnwandInfos.add(nutzerNameLabel);
-		aboPinnwandInfos.add(nickNameLabel);
+//		aboPinnwandInfos.add(nutzerNameLabel);
+//		aboPinnwandInfos.add(nickNameLabel);
 		
 		ReportButtonPanel.add(beitragStatistikButton);
 		ReportButtonPanel.add(nutzerStatistikButton);
 		
+		/**
+		 * 
+		 */
 		profil.addStyleName("profil");
 		anzeigePW.addStyleName("anzeigePW");
 		aboErstellBtn.addStyleName("aboErstellBtn");
@@ -97,24 +107,115 @@ public class AboBox extends VerticalPanel{
 		beitragStatistikButton.addStyleName("beitragStatistikButton");
 		nutzerStatistikButton.addStyleName("nutzerStatistikButton");
 		
+		/**
+		 * 
+		 */
+		aboErstellBtn.addClickHandler(new aboErstellBtnClickHandler());
+		deAboBtn.addClickHandler(new deAboBtnClickHandler());
+		
+		nutzerNameLabel.addClickHandler(new PinnwandAnzeigen());
+		nickNameLabel.addClickHandler(new PinnwandAnzeigen());
+		
+		beitragStatistikButton.addClickHandler(new BeitragStatistikClickHandler());
+		nutzerStatistikButton.addClickHandler(new NutzerStatistikClickHandler());
+		
 		RootPanel.get("AboDiv").add(abobox);
-		
-		
-//		//for(int i = 0; i < pinnwandVerwaltung.getAllAbosFor(n, null); i++) {
-//			AboPinnwandBox tempAboPinnwandBox = new AboPinnwandBox(i);
-//			nutzerAbos.add(i, tempAboPinnwandBox);
-//		}
-		
-//		for(int i = 0; i < nutzerAbos.size(); i++) {
-//			this.add(nutzerAbos.elementAt(i));
-//		}
-		
 		
 	}
 	
-	//Implementierung: findAllAbosBy, die eine Liste an Abonnementbeziehungen des angemeldetenNutzers liefern,
-	// Id Abgleich dieser Liste, ob jeweilige Pinnwand ID bereits dem VP geaddet wurde (if-Abfrage) 
-	//Nutzen der Methode: getAllAbosByNutzer
+	class aboErstellBtnClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			// zunächst if Abfrage: if nutzer == cookie nutzer -> button hide oder keine event, else -> abo erstellen
+			
+			Nutzer n = new Nutzer();
+			
+//			n.setId(Integer.parseInt(Cookies.getCookie("id")));
+			
+			n.setId(1);
+			
+			Pinnwand p = new Pinnwand();
+			p.setNutzerFK(n.getId());
+			
+			Timestamp erstellzeitpunkt = null;
+			
+			pinnwandVerwaltung.erstelleAbonnement(p, n, erstellzeitpunkt, new AbonnementAnlegenCallBack());
+			
+		}
+		
+	}
 	
+	public class AbonnementAnlegenCallBack implements AsyncCallback<Abonnement> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Fehler beim Anlegen des Abonnements: " + caught.getMessage());
+			
+		}
+
+		@Override
+		public void onSuccess(Abonnement result) {
+			Window.alert("Das Abonnement wurde erfolgreich angelegt.");
+			Nutzer n = new Nutzer();
+			
+			n.setId(result.getNutzerFK());
+			
+			String nutzerNameString = n.getVorname();
+			nutzerNameLabel.setText(nutzerNameString);
+			
+			String nickNameString = n.getNickname();
+			nickNameLabel.setText(nickNameString);
+			
+			aboPinnwandInfos.add(nutzerNameLabel);
+			aboPinnwandInfos.add(nickNameLabel);
+			
+			aboniertePW.add(aboPinnwandInfos);
+			
+		}
+		
+	}
+	
+	
+	class deAboBtnClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			
+		} 
+		
+	}
+	
+	class PinnwandAnzeigen implements ClickHandler {
+		
+		
+		public void onClick(ClickEvent event) {
+			
+			
+		}
+		
+	}
+	
+	class BeitragStatistikClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	class NutzerStatistikClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 	
 }
