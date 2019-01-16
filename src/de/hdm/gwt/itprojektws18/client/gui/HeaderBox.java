372274c0ce24b07b2e5a58fcb4c48474b7cec38a
FlorianTimmler <florian.timmler@gmx.de>
2019-01-16 19:48:08 +0100

    - Nutzer über UI löschen möglich
    - CSS Anpassungen/Tests
    - DialogBox für Profilbearbeitung erweitert
    - ProfilBearbeitenBox neu erstellt

--- src/de/hdm/gwt/itprojektws18/client/gui/HeaderBox.java
+++ src/de/hdm/gwt/itprojektws18/client/gui/HeaderBox.java
@@ -17,54 +17,47 @@ import de.hdm.gwt.itprojektws18.shared.bo.Nutzer;
 public class HeaderBox extends HorizontalPanel {
 
 	PinnwandVerwaltungAsync pinnwandVerwaltung = ClientsideSettings.getPinnwandVerwaltung();
-	
-	
-	private HorizontalPanel inhalte = new HorizontalPanel();
+
 	private ProfilBox profilBox = new ProfilBox();
 	private Suchleiste suchLeiste = new Suchleiste();
 	private VerticalPanel logoutEditPanel = new VerticalPanel();
 
 	private Button logoutButton = new Button("Logout");
-	private Button profilEditButton = new Button("Profil bearbeiten");
+	private Button profilEditButton = new Button("Mein Profil bearbeiten");
 
-	private Label nicknameLbl = new Label("Neuer Nickname: ");
-	private Label vornameLbl = new Label("Neuer Vorname: ");
-	private Label nachnameLbl = new Label("Neuer Nachname: ");
+	private Label deleteCheckLbl = new Label("Sind Sie sich sicher, dass Sie Ihr Nutzerprofil und" + "\n"
+			+ "alle damit in Zusammenhang stehende Daten unwiderruflich löschen wollen?");
 
-	private TextArea nicknameTextbox = new TextArea();
-	private TextBox vornameTextbox = new TextBox();
-	private TextBox nachnameTextbox = new TextBox();
+	private Button openDeleteFormButton = new Button("Profil löschen");
+	private Button deleteButton = new Button("Nutzerdaten endgültig löschen");
+	private Button abortDeletionButton = new Button("Abbruch");
 
-	private Button changeButton = new Button("Aenderungen speichern");
-	private Button closeButton = new Button("Schließen");
-
-	private EditForm dlgBox = new EditForm();
-
-	
-
+	private DeleteForm deleteBox = new DeleteForm();
 
 	public HeaderBox() {
 
-	
 		this.addStyleName("headerBox");
 
-//		headerBox.add(EditPanel);
-//		headerBox.add(inhalte);
+		this.add(profilBox);
+		this.add(suchLeiste);
+		this.add(logoutEditPanel);
 
-		inhalte.add(profilBox);
-		inhalte.add(suchLeiste);
-		inhalte.add(logoutEditPanel);
+		profilBox.setHorizontalAlignment(ALIGN_LEFT);
+		suchLeiste.setHorizontalAlignment(ALIGN_CENTER);
+		logoutEditPanel.setHorizontalAlignment(ALIGN_RIGHT);
 
 		logoutEditPanel.add(logoutButton);
 		logoutEditPanel.add(profilEditButton);
+		logoutEditPanel.add(openDeleteFormButton);
+
+		openDeleteFormButton.addClickHandler(new OpenDeleteFormClickHandler());
 
 		logoutEditPanel.addStyleName("logoutEditPanel");
 		logoutButton.addStyleName("logoutButton");
-//		ProfilEditButton.addStyleName("profilEditButton");
+		profilEditButton.addStyleName("profilEditButton");
+		openDeleteFormButton.addStyleName("openDeleteFormButton");
 
 		profilEditButton.addClickHandler(new ChangeClickHandler());
-		
-		this.add(inhalte);
 
 		super.onLoad();
 	}
@@ -73,78 +66,100 @@ public class HeaderBox extends HorizontalPanel {
 
 		public void onClick(ClickEvent event) {
 
-			dlgBox.center();
-
-		}
-
-	}
-
-	private class EditForm extends DialogBox {
-		private VerticalPanel editPanel = new VerticalPanel();
-
-		public EditForm() {
-
-			closeButton.addClickHandler(new CloseClickHandler());
-
-//			/**
-//			 * Einfügen der in der Datenbank bereits vorhandenen Informationen
-//			 */
-//			vornameTextbox.setText("");
-//			nachnameTextbox.setText("");
-//			nicknameTextbox.setText("");
-
-			editPanel.add(nicknameLbl);
-			editPanel.add(nicknameTextbox);
-			editPanel.add(vornameLbl);
-			editPanel.add(vornameTextbox);
-			editPanel.add(nachnameLbl);
-			editPanel.add(nachnameTextbox);
-
-			editPanel.add(changeButton);
-			editPanel.add(closeButton);
-			this.add(editPanel);
-			changeButton.addClickHandler(new ProfilEditClickHandler());
-		}
-
-	}
-
-	class ProfilEditClickHandler implements ClickHandler {
-
-		public void onClick(ClickEvent event) {
-
-			Nutzer n = new Nutzer();
-
+			final Nutzer n = new Nutzer();
 			// n.setId(Integer.parseInt(Cookies.getCookie("id")));
 			n.setId(3);
-			n.setVorname(vornameTextbox.getText());
-			n.setNachname(nachnameTextbox.getText());
-			n.setNickname(nicknameTextbox.getText());
+			pinnwandVerwaltung.getNutzerbyID(n.getId(), new AsyncCallback<Nutzer>() {
 
-			pinnwandVerwaltung.speichern(n, new NutzerEditCallback());
+				@Override
+				public void onFailure(Throwable caught) {
+					Window.alert("Fehler beim Auslesen der Nutzerdaten" + caught.getMessage());
+
+				}
+
+				@Override
+				public void onSuccess(Nutzer result) {
+					ProfilBearbeitenDialogBox profilEditBox = new ProfilBearbeitenDialogBox(result);
+					profilEditBox.center();
+				}
+
+			});
+		}
+
+	}
+
+	// Schablone der DialogBox, die das Löschen des Profils ermöglicht
+	private class DeleteForm extends DialogBox {
+		private VerticalPanel deletePanel = new VerticalPanel();
+
+		public DeleteForm() {
+
+			deleteButton.addClickHandler(new DeleteProfilClickHandler());
+			abortDeletionButton.addClickHandler(new AbortDeletionClickHandler());
+
+			deletePanel.add(deleteCheckLbl);
+			deletePanel.add(deleteButton);
+			deletePanel.add(abortDeletionButton);
+			this.add(deletePanel);
 
 		}
 	}
 
-	public class NutzerEditCallback implements AsyncCallback<Void> {
+	// ClickHandler, der das Öffnen der DeleteForm zum Löschen des Profils
+	// ermöglicht
+	class OpenDeleteFormClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+//			dlgBox.hide();
+			deleteBox.center();
+
+		}
+
+	}
+
+	// ClickHandler, der die Löschung des Profils des eingeloggten Nutzers
+	// durchführt
+	class DeleteProfilClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+
+			// Später Cookie Id anstatt Id = 3
+			Nutzer n = new Nutzer();
+			// n.setId(Integer.parseInt(Cookies.getCookie("id")));
+			n.setId(3);
+			pinnwandVerwaltung.loeschen(n, new DeleteNutzerCallback());
+
+		}
+
+	}
+
+	public class DeleteNutzerCallback implements AsyncCallback<Void> {
 
 		@Override
 		public void onFailure(Throwable caught) {
-			Window.alert("Fehler beim Bearbeiten des Nutzers: " + caught.getMessage());
+			Window.alert("Fehler beim Löschen des Profils" + caught.getMessage());
 
 		}
 
 		@Override
 		public void onSuccess(Void result) {
-			Window.alert("Nutzer wurde erfolgreich geändert");
+			Window.alert("Ihr Profil wurde erfolgreich gelöscht!");
+			// Laden der Login-Seie ausstehend
+
 		}
 
 	}
 
-	class CloseClickHandler implements ClickHandler {
+	class AbortDeletionClickHandler implements ClickHandler {
 
+		@Override
 		public void onClick(ClickEvent event) {
-			dlgBox.hide();
+			deleteBox.hide();
+
 		}
 
 	}
+
 }
